fcab57fae21a2863a18cf1abe2f79df83e1bb106
SiddharthV147 <siddharthvarade147@gmail.com>
2026-05-31 22:46:32 +0530

    update

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 require("config.lazy")
-vim.opt.tabstop = 4 -- tab character width
-vim.opt.shiftwidth = 4 -- indent width (>> / <<)
+vim.opt.tabstop = 2 -- tab character width
+vim.opt.shiftwidth = 2 -- indent width (>> / <<)
 vim.opt.expandtab = true -- use spaces instead of tab characters
